1f89e745511f15976ce7be13643f1121d04a5444
s-dub <ckl15076@nitech.jp>
2020-02-03 15:01:08 +0900

    complete ch6-2

---
+++ ch6/scripts/element_name.js
@@ -0,0 +1,5 @@
+var current = new Date();
+
+var nam = document.getElementsByName('time');
+
+nam[0].value = current.toLocaleTimeString();
---
+++ ch6/scripts/first_chile_element.js
@@ -0,0 +1,8 @@
+var s = document.getElementById('food');
+
+var child = s.firstElementChild;
+
+while (child) {
+    console.log(child.value);
+    child = child.nextElementSibling;
+}
